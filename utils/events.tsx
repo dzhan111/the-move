163f7e99e472989f32c1f16fc2a8f1906f37095b
dzhan111 <zhandavid4@gmail.com>
2025-03-25 21:57:50 -0400

    new events

--- utils/events.tsx
+++ utils/events.tsx
@@ -1,5 +1,17 @@
 export const allEvents = [
-    { title: 'Solo Leveling Watch Party', description: 'Watch Anime and shit', date: 'Saturday Mar 29 @9pm', location:'Sam House'},
+
+
+    { title: 'Munchy Brunch', 
+    description: 'Hee Hee Hee haw',
+    date: 'Sunday March 29 @10am', 
+    location:'Landmark Americana'},
+
+    { title: 'Solo Leveling Watch Party', 
+      description: 'Watch Anime and goon',
+      date: 'Saturday Mar 29 @9pm', 
+      location:'Sam House'},
+
+    
     { title: 'Hide and seek', description: 'Hide and seek in Towne Building', date: 'Friday Mar 21 @9pm', location:'Towne Building' },
     { title: 'meet with the megang', description: 'Italy', date: 'Saturday Mar 16 @9pm', location:'al parlamento' },
     { title: 'big big butt', description: 'da pi gu', date: 'Friday Mar 21 @9pm', location:'Sam House' },
